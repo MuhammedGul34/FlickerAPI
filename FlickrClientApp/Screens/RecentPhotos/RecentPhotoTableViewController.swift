ea27519ca6b6b137905be6304157b3eaa9fb3024
MuhammedGul34 <muhammedgul.mail@gmail.com>
2022-12-14 17:33:37 +0300

    Seting up UI

---
+++ FlickrClientApp/Screens/RecentPhotos/RecentPhotoTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RecentPhotoTableViewController.swift
+//  FlickrClientApp
+//
+//  Created by Muhammed Gül on 13.12.2022.
+//
+
+import UIKit
+
+class RecentPhotoTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
